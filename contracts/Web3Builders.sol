1c246a262f55c8001a15b287f481422e1ff2361a
Arum-Favour <nwajiudo@gmail.com>
2024-12-10 16:22:28 +0100

    Added public and allow list mint features

--- contracts/Web3Builders.sol
+++ contracts/Web3Builders.sol
@@ -9,7 +9,11 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Kingsx is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
     uint256 private _nextTokenId;
-    uint256 maxSupply = 1;
+    uint256 maxSupply = 2000;
+    bool public publicMintOpen = false;
+    bool public allowListMintOpen = false;
+
+    mapping (address => bool) public allowList;
 
     constructor(address initialOwner)
         ERC721("Kingsx", "KNX")
@@ -28,16 +32,46 @@ contract Kingsx is ERC721, ERC721Enumerable, ERC721Pausable, Ownable {
         _unpause();
     }
 
-    function publicMint() public payable  {
+    function editMintWindow(bool _publicMintOpen,
+        bool _allowListMintOpen)external onlyOwner{
+             publicMintOpen = _publicMintOpen;
+             allowListMintOpen = _allowListMintOpen;
+        }
+        
+
+    function allowListMint() public payable {
+        require(allowListMintOpen," Allow List Mint Closed!");
+        require(allowList[msg.sender], "You are not on the allow list");
+        require(msg.value == 0.001 ether, "Not enough funds");
+        internalMint();
+    }
+
+    function publicMint() public payable {
+        require(publicMintOpen, "Public mint has ended");
         require(msg.value == 0.01 ether, "Not enough funds");
-        require(totalSupply() < maxSupply, "We Sold Out!");
-        uint256 tokenId = _nextTokenId++;
+        internalMint();
+    }
+
+    function setAllowList(address[] calldata addresses)external onlyOwner{
+       for (uint256 i = 0; i < addresses.length; i++)
+       {
+        allowList[addresses[i]] = true;
+       }
+    }
+
+    function internalMint() internal() {
+         require(totalSupply() < maxSupply, "We Sold Out!");
+         uint256 tokenId = _nextTokenId++;
         _safeMint(msg.sender, tokenId);
     }
 
     // The following functions are overrides required by Solidity.
 
-    function _update(address to, uint256 tokenId, address auth)
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    )
         internal
         override(ERC721, ERC721Enumerable, ERC721Pausable)
         returns (address)
